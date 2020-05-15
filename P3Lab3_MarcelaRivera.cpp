927a5b38f1719baed7f3095c4b5825c80075e2d9
Marcela <mvrivera@unitec.edu>
2020-05-15 15:54:10 -0600

    das

--- P3Lab3_MarcelaRivera.cpp
+++ P3Lab3_MarcelaRivera.cpp
@@ -9,6 +9,7 @@ using std::cout;
 void ejercicio1();
 void ejercicio2();
 void imprimir(int ** matrix,int x,int y);
+void juego(int**matrix ,int x, int y,int turnos);
 
 int main(){
 		while(true){
@@ -32,23 +33,28 @@ int main(){
 }
 
 void imprimir(int ** matrix,int x,int y){
-	for(int i=0;i<x;i++){
-		cout<<" # ";
-	}
+for(int i=0;i<x+1;i++){
+	cout<<"#  ";
+}
 	for(int i=0;i<x;i++){
 		for(int j=0;j<y;j++){
-			
+			if(j==0){
+				cout<<"# ";
+			}
 			if(matrix[i][j]==1){
+				
 			cout<<" * ";	
 			}else{
 				cout<<"   ";
 			}
 		}
-		cout<<endl;
+		
+		cout<<" #"<<endl;
 	}
-	for(int i=0;i<x;i++){
-		cout<<" # ";
+	for(int i=0;i<x+1;i++){
+	cout<<"#  ";
 	}
+
 	cout<<endl;
 }
 
@@ -61,11 +67,10 @@ void ejercicio1(){
 	cout<<"Ingrese la cantidad de turnos"<<endl<<": ";
 	cin>>turnos;
 	int ** matrix=new int*[y];
-	int ** auxmatrix=new int*[y];
 	int num;
 	for(int i=0;i<x;i++){
 		matrix[i]=new int[x];
-		auxmatrix[i]=new int[x];
+		
 		for(int j=0;j<y;j++){
 			num=0+rand()%(2-0);
 			if(num==0){
@@ -73,15 +78,17 @@ void ejercicio1(){
 			}else{
 			matrix[i][j]=1;	
 			}
-			auxmatrix[i][j]=0;
+		
 		}
 	}
 	imprimir(matrix,x,y);
-	
-	
+	juego(matrix,x,y,turnos);	
 }
 
 void ejercicio2(){
+int turnos;
+cout<<"Ingrese la cantidad de turnos"<<endl<<": ";
+cin>>turnos;
 int x = 20;
 int y = 20;
 int temp_mat [20][20] = {
@@ -113,13 +120,99 @@ int temp_mat [20][20] = {
 			mat[i][j] = temp_mat[i][j];
 		}
 	}
-	imprimir(mat,x,y);
-	
-	
-	
+	imprimir(mat,x,y);	
+	juego(mat,x,y,turnos);
 }
 
-
+void juego(int**matrix ,int x, int y,int turnos){
+	int ** aux=new int*[y];
+	for(int i=0;i<x;i++){
+		aux[i]=new int[x];
+		for(int j=0;j<y;j++){
+			aux[i][j]=0;
+		}
+	}
+	int c=0;
+	cout<<"turnos: "<<turnos<<endl;
+while(c<turnos){
+		int cont=0;
+		for(int j=1;j<x;j++){
+			for(int m=1;m<y;m++){
+				if(m<=y-1){
+					if(matrix[j][m+1]==1){
+						cont++;
+						//cout<<"conto 1"<<endl;
+					}
+				}
+				if(j<=x-1){
+					if(matrix[j+1][m]==1){
+						cont++;
+						//cout<<"conto 2"<<endl;
+					}
+				}
+				if(j<=x-1 && m<=y-1){
+					if(matrix[j+1][m+1]==1){
+						cont++;
+						//cout<<"conto 3"<<endl;
+					}
+				}
+				if(j>0){
+					if(matrix[j-1][m+1]==1){
+						cont++;
+					//	cout<<"conto 4"<<endl;
+					}
+					if(matrix[j-1][m]){
+						cont++;
+						//cout<<"conto 5"<<endl;
+					}
+				}
+				if(m>0){
+					if(matrix[j-1][m-1]){
+						cont++;
+					//	cout<<"conto 6"<<endl;
+					}
+					if(matrix[j][m-1]==1){
+						cont++;
+						//cout<<"conto 7"<<endl;
+					}
+				}
+				if(j<x-1&&m>0){
+					if(matrix[j+1][m-1]==1){
+						cont++;
+						//cout<<"conto 8"<<endl;
+					}
+				}
+				if(cont<2&&matrix[j][m]==1){
+					aux[j][m]=0;
+					cout<<"muere"<<endl;
+				}
+				if((cont==2||cont==3) && matrix[j][m]==1){
+					aux[j][m]=1;
+					cout<<"vive"<<endl;
+				}
+				if(cont>3&&matrix[j][m]==1){
+					aux[j][m]=0;
+					cout<<"muere"<<endl;
+				}
+				if(cont==3&&matrix[j][m]==0){
+					aux[j][m]=1;
+					cout<<"vive"<<endl;
+				}
+				
+				cont=0;	
+			}
+		}
+		
+		for(int e=0;e<x;e++){
+			for(int r=0;r<y;r++){
+				matrix[e][r]=aux[e][r];
+				aux[e][r]=0;
+				}
+		}			
+		imprimir(matrix,x,y);
+		c++;
+	}
+}
 
 
 
